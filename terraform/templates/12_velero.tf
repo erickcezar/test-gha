255f77794e8f0ea0dc2ca0fcadc347a6071d65f5
Erick Oliveira <erick@vmfarms.com>
2022-10-14 09:24:53 -0400

    Changes

--- terraform/templates/12_velero.tf
+++ terraform/templates/12_velero.tf
@@ -14,7 +14,7 @@ module "velero" {
   AWS_ACCESS_KEY_ID             = var.AWS_ACCESS_KEY_ID
   AWS_SECRET_ACCESS_KEY         = var.AWS_SECRET_ACCESS_KEY
   aws_bucket_region             = "ca-central-1"
-  aws_bucket_name               = "marketcircle-tor1-${local.environment}-velero-backups"
+  aws_bucket_name               = "marketcircle-${local.velero_region}-${local.environment}-velero-backups"
   aws_bucket_versioning_enabled = false
   aws_snapshots_enabled         = true        #Snapshots are stored in the Source cloud (DigitalOcean)
   aws_velero_schedule           = "0 7 * * *" #UTC Time
@@ -23,7 +23,7 @@ module "velero" {
   do_spaces_key                                 = var.DO_SPACES_KEY
   do_spaces_secret                              = var.DO_SPACES_SECRET
   digitalocean_bucket_region                    = "nyc3"
-  digitalocean_bucket_name                      = "marketcircle-tor1-${local.environment}-velero-backups"
+  digitalocean_bucket_name                      = "marketcircle-${local.velero_region}-${local.environment}-velero-backups"
   digitalocean_bucket_versioning_enabled        = false
   digitalocean_bucket_lifecycle_enabled         = true
   digitalocean_bucket_lifecycle_expiration_days = "60"
